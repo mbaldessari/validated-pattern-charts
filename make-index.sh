1fb4084e608369515f9f1db01bc31f598431d019
Michele Baldessari <michele@acksyn.org>
2022-09-27 20:34:06 +0200

    Add make-index to generate everything

---
+++ make-index.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+helm lint charts-source/*
+
+helm package charts-source/* -u -d charts
+
+#helm repo index --url https://hybrid-cloud-patterns.github.io/validated-pattern-charts/ . 
+helm repo index --url https://hybrid-cloud-patterns.github.io/validated-pattern-charts/ --merge index.yaml .
