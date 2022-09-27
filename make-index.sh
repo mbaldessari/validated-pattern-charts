47b2c86b2c42be73a9b1fe9521b01b6d1425f3d1
Michele Baldessari <michele@acksyn.org>
2022-09-27 20:39:01 +0200

    New release

--- make-index.sh
+++ make-index.sh
@@ -5,4 +5,4 @@ helm lint charts-source/*
 helm package charts-source/* -u -d charts
 
 #helm repo index --url https://hybrid-cloud-patterns.github.io/validated-pattern-charts/ . 
-helm repo index --url https://hybrid-cloud-patterns.github.io/validated-pattern-charts/ --merge index.yaml .
+helm repo index --url https://mbaldessari.github.io/validated-pattern-charts/ --merge index.yaml .
